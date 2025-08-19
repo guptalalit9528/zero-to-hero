4c622846335f3a78085b2d9d3b19528c1b046e1d
Lalit <lalitgupta1qq@gmail.com>
2025-08-19 08:07:18 +0530

    counting

---
+++ src/main/java/coreJava/CountingWhileLoop.java
@@ -0,0 +1,18 @@
+package coreJava;
+
+public class CountingWhileLoop {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 50) {
+            System.out.print(i + " ");
+            i++;
+        }
+        System.out.println();
+        int j = 51;
+        while (j <= 100) {
+            System.out.print(j + " ");
+            j++;
+
+        }
+    }
+}
---
+++ src/main/java/coreJava/ReverseCounting.java
@@ -0,0 +1,12 @@
+package coreJava;
+
+public class ReverseCounting {
+    public static void main(String[] args) {
+      int num=100;
+      while(num>=1){
+        System.out.print(+num +"  ");
+        num--;
+      }
+        System.out.println();
+    }
+}
